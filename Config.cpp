9261e4900454007d397038a189a47a464573d552
nongeneric <nongeneric@users.noreply.github.com>
2013-09-26 20:45:47 +0400

    change config format

--- Config.cpp
+++ Config.cpp
@@ -4,15 +4,12 @@
 #include <boost/property_tree/xml_parser.hpp>
 
 /*
-<tetris>
-    <orthographic>true</orthographic>
-    <fullscreen>true</fullscreen>
-    <showFps>false</showFps>
-    <initialLevel>17</initialLevel>
-    <resolution>
-        <width>1920</width>
-        <height>1080</height>
-    </resolution>
+<tetris orthographic="true"
+        fullscreen="true"
+        showFps="false"
+        initialLevel="17">
+    <resolution width="1920"
+                height="1080"/>
 </tetris>
 */
 
@@ -20,10 +17,10 @@ void TetrisConfig::load(const std::string &fileName) {
     using boost::property_tree::ptree;
     ptree pt;
     read_xml(fileName, pt);
-    orthographic = pt.get("tetris.orthographic", true);
-    fullScreen = pt.get("tetris.fullscreen", false);
-    screenWidth = pt.get("tetris.resolution.width", 600);
-    screenHeight = pt.get("tetris.resolution.height", 600);
-    showFps = pt.get("tetris.showFps", false);
-    initialLevel = pt.get("tetris.initialLevel", 0);
+    orthographic = pt.get("tetris.<xmlattr>.orthographic", true);
+    fullScreen = pt.get("tetris.<xmlattr>.fullscreen", false);
+    screenWidth = pt.get("tetris.resolution.<xmlattr>.width", 600);
+    screenHeight = pt.get("tetris.resolution.<xmlattr>.height", 600);
+    showFps = pt.get("tetris.<xmlattr>.showFps", false);
+    initialLevel = pt.get("tetris.<xmlattr>.initialLevel", 0);
 }
